c641834835164f9effd2246f9cdd4ae613d8071e
Akenaide <kevinms1@gmail.com>
2016-03-15 00:18:23 +0100

    Wait all goroutines are done

--- main.go
+++ main.go
@@ -9,49 +9,21 @@ import (
 	"path"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-type API struct {
-	Message string `json:"message"`
-}
-
 const yuyuteiURL = "http://yuyu-tei.jp/"
-
-func getImage(url string, dirName string) {
-	currentDir, _ := os.Getwd()
-
-	dir := filepath.Join(currentDir, dirName)
-	filePath := filepath.Join(dir, path.Base(url))
-	os.MkdirAll(dir, 0744)
-	out, err := os.Create(filePath)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer out.Close()
-	reps, err := http.Get(url)
-
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	file, err := io.Copy(out, reps.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	fmt.Println("File", file)
-	// fmt.Printf("Link: n-%d __ %v%v\n", i, imageURL, uid)
-	defer reps.Body.Close()
-}
 
 func main() {
 	http.HandleFunc("/cardimages", func(w http.ResponseWriter, r *http.Request) {
+		var wg sync.WaitGroup
+
 		link := r.PostFormValue("url")
-		class := r.PostFormValue("class")
-		if class == "" {
-			class = ".card_list_box"
+		classCSS := r.PostFormValue("class_css")
+		if classCSS == "" {
+			classCSS = ".card_list_box"
 		}
 		doc, err := goquery.NewDocument(link)
 		parsedURL, _ := url.Parse(link)
@@ -62,14 +34,44 @@ func main() {
 			fmt.Println("Nope")
 		}
 
-		doc.Find(class + " img").Each(func(i int, s *goquery.Selection) {
+		doc.Find(classCSS + " .image img").Each(func(i int, s *goquery.Selection) {
+			wg.Add(1)
 			val, _ := s.Attr("src")
 			big := strings.Replace(val, "90_126", "front", 1)
 			imageURL := yuyuteiURL + big
 
-			go getImage(imageURL, uid)
+			go func(url string, dirName string) {
+				defer wg.Done()
+				currentDir, _ := os.Getwd()
+
+				dir := filepath.Join(currentDir, dirName)
+				filePath := filepath.Join(dir, path.Base(url))
+				os.MkdirAll(dir, 0744)
+				out, err := os.Create(filePath)
+				if err != nil {
+					fmt.Println(err)
+				}
+				defer out.Close()
+				reps, err := http.Get(url)
+
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				file, err := io.Copy(out, reps.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+
+				fmt.Println("File", file)
+				// fmt.Printf("Link: n-%d __ %v%v\n", i, imageURL, uid)
+				defer reps.Body.Close()
+			}(imageURL, uid)
 
 		})
+
+		wg.Wait()
+		fmt.Printf("Finish")
 	})
 	http.ListenAndServe(":8010", nil)
 }
