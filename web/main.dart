ec1a6fce9c30d0cb729d2a34ffef3e703a236fff
Kevin Man Sang <kevin.mansang@mhcomm.fr>
2016-03-14 13:59:42 +0100

    Quick way to show pdf

--- web/main.dart
+++ web/main.dart
@@ -3,6 +3,33 @@
 
 import 'dart:html';
 
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+
+// getCardImages(String url) {
+//   HttpRequest.requestCrossOrigin(url).then((HttpRequest response) {
+//     var document = parse(response.body);
+//     var images = document.querySelectorAll(".card_list_box img");
+//   });
+
+//   querySelector('#output').append(images);
+// }
+
+getPdfFile() {
+  print(querySelector("#input-dir").files);
+  for (var file in querySelector("#input-dir").files) {
+    var reader = new FileReader();
+    reader.onLoad.listen((e) {
+      var thumbnail = new Element.tag("embed");
+      thumbnail.src = reader.result;
+      querySelector('#output').append(thumbnail);
+    });
+    reader.readAsDataUrl(file);
+  }
+}
+
 void main() {
   querySelector('#output').text = 'Your Dart app is running.';
+  querySelector("#input-dir").onChange.listen((e) => getPdfFile());
+//   querySelector("#enterUrl").onclick.listen((event) {});
 }
