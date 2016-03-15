c9914ef5843697ba47bb3fb34f53a22af98f68e4
Kevin Man Sang <kevin.mansang@mhcomm.fr>
2016-03-15 13:27:23 +0100

    Do nothing if link is not given

--- main.go
+++ main.go
@@ -46,53 +46,56 @@ func main() {
 
 		link := r.PostFormValue("url")
 		classCSS := r.PostFormValue("class_css")
+
 		if classCSS == "" {
 			classCSS = ".card_list_box"
 		}
-		doc, err := goquery.NewDocument(link)
-		parsedURL, _ := url.Parse(link)
-		values, _ := url.ParseQuery(parsedURL.RawQuery)
-		uid := values.Get("ver")
 
-		if err != nil {
-			fmt.Println("Nope")
+		if link != "" {
+			doc, err := goquery.NewDocument(link)
+			parsedURL, _ := url.Parse(link)
+			values, _ := url.ParseQuery(parsedURL.RawQuery)
+			uid := values.Get("ver")
+
+			if err != nil {
+				fmt.Println("Nope")
+			}
+
+			doc.Find(classCSS + " .image img").Each(func(i int, s *goquery.Selection) {
+				wg.Add(1)
+				val, _ := s.Attr("src")
+				big := strings.Replace(val, "90_126", "front", 1)
+				imageURL := yuyuteiURL + big
+
+				go func(url string, dirName string) {
+					defer wg.Done()
+					currentDir, _ := os.Getwd()
+
+					dir := filepath.Join(currentDir, dirName)
+					filePath := filepath.Join(dir, path.Base(url))
+					os.MkdirAll(dir, 0744)
+					out, err := os.Create(filePath)
+					if err != nil {
+						fmt.Println(err)
+					}
+					defer out.Close()
+					reps, err := http.Get(url)
+
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					file, err := io.Copy(out, reps.Body)
+					if err != nil {
+						fmt.Println(err)
+					}
+
+					fmt.Println("File", file)
+					// fmt.Printf("Link: n-%d __ %v%v\n", i, imageURL, uid)
+					defer reps.Body.Close()
+				}(imageURL, uid)
+			})
 		}
-
-		doc.Find(classCSS + " .image img").Each(func(i int, s *goquery.Selection) {
-			wg.Add(1)
-			val, _ := s.Attr("src")
-			big := strings.Replace(val, "90_126", "front", 1)
-			imageURL := yuyuteiURL + big
-
-			go func(url string, dirName string) {
-				defer wg.Done()
-				currentDir, _ := os.Getwd()
-
-				dir := filepath.Join(currentDir, dirName)
-				filePath := filepath.Join(dir, path.Base(url))
-				os.MkdirAll(dir, 0744)
-				out, err := os.Create(filePath)
-				if err != nil {
-					fmt.Println(err)
-				}
-				defer out.Close()
-				reps, err := http.Get(url)
-
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				file, err := io.Copy(out, reps.Body)
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				fmt.Println("File", file)
-				// fmt.Printf("Link: n-%d __ %v%v\n", i, imageURL, uid)
-				defer reps.Body.Close()
-			}(imageURL, uid)
-
-		})
 
 		wg.Wait()
 		fmt.Printf("Finish")
