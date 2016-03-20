3ee69b96383dbc21aff1a418bccdf11dab9c3199
Akenaide <kevinms1@gmail.com>
2016-03-20 02:50:17 +0100

    Allow to drop cropped canvas into card canvas

--- web/main.dart
+++ web/main.dart
@@ -13,33 +13,49 @@ allowDrop(Event event) {
 }
 
 drag(MouseEvent event) {
-  print("YAy ====");
+  print(event.target.id);
   event.dataTransfer.setData("text", event.target.id);
 }
 
 drop(MouseEvent event) {
   event.preventDefault();
-  var data = event.dataTransfer.getData("text");
+  String data = event.dataTransfer.getData("text");
+  print("hey");
   print(data);
-  print(querySelector("#"+data));
-  event.target.append(querySelector("#"+data));
+  CanvasElement canvas = event.target;
+  CanvasRenderingContext2D context = canvas.getContext("2d");
+  CanvasElement sourceCanvas = querySelector("#"+data);
+  context.drawImageScaled(sourceCanvas, 0, canvas.height - sourceCanvas.height, canvas.width, sourceCanvas.height);
 }
 
 addImage(event) {
-  var image = new ImageElement()
-    ..src = event.target.src
-    ..classes.add("image")
-    ..onDragOver.listen((e) => allowDrop(e))
-    ..onDrop.listen((e) => drop(e));
 
-  querySelector('#output').append(image);
-  querySelectorAll('#output img').onClick.listen((e) => removeImage(e));
+  var image = new ImageElement()
+    ..src = event.target.src;
+
+  CanvasElement canvas = new CanvasElement()
+   ..classes.add("image")
+   ..onDragOver.listen((e) => allowDrop(e))
+   ..onDrop.listen((e) => drop(e));
+
+   image.onLoad.listen((e) {
+    CanvasRenderingContext2D context = canvas.getContext("2d");
+    canvas
+      ..width = image.width
+      ..height = image.height;
+
+    context.drawImage(image, 0, 0);
+   });
+
+
+  querySelector('#output').append(canvas);
+  querySelectorAll('#output div.image').onClick.listen((e) => removeImage(e));
 }
 
 getCardImages() {
-  InputElement url = querySelector("#url").value;
+  InputElement url = querySelector("#url");
   var output = querySelector('#images-box');
-  HttpRequest.postFormData("/cardimages", {"url": url}).then((HttpRequest response) {
+  HttpRequest.postFormData("/cardimages", {"url": url.value}).then((HttpRequest response) {
     List parsedList = JSON.decode(response.response);
     for (var url in parsedList) {
       var image = new ImageElement();
@@ -50,23 +66,60 @@ getCardImages() {
   });
 }
 
-getPdfFile() {
-  print(querySelector("#input-dir").files);
-  for (var file in querySelector("#input-dir").files) {
-    var reader = new FileReader();
-    reader.onLoad.listen((e) {
-      var thumbnail = new Element.tag("embed");
-      thumbnail.src = reader.result;
-      querySelector('#output').append(thumbnail);
-    });
-    reader.readAsDataUrl(file);
-  }
+// getPdfFile() {
+//   print(querySelector("#input-dir").files);
+//   for (var file in querySelector("#input-dir").files) {
+//     var reader = new FileReader();
+//     reader.onLoad.listen((e) {
+//       var thumbnail = new Element.tag("embed");
+//       thumbnail.src = reader.result;
+//       querySelector('#output').append(thumbnail);
+//     });
+//     reader.readAsDataUrl(file);
+//   }
+// }
+
+initCanvas(ImageElement img, CanvasElement canvas) {
+  CanvasElement outCanvas = new CanvasElement();
+  CanvasRenderingContext2D outContext = outCanvas.getContext('2d');
+  CanvasRenderingContext2D context = canvas.getContext('2d');
+  List points = [];
+
+  canvas
+    ..width = img.width
+    ..height = img.height;
+  // context.drawImageScaled(img, 0, 0, 55, 50);
+  context.drawImage(img, 0, 0);
+
+  canvas.onMouseDown.listen((e) {
+    points.add(e.offset);
+    if (points.length == 2) {
+      outContext.clearRect(0, 0, outCanvas.width, outCanvas.height);
+      Rectangle rect = new Rectangle.fromPoints(points[0], points[1]);
+      outCanvas
+        ..attributes.addAll({"draggable": true})
+        ..id = "yay"
+        // ..id = new DateTime.now().millisecondsSinceEpoch.toString()
+        ..onDragStart.listen((e) => drag(e))
+        ..width = rect.width
+        ..height = rect.height;
+      outContext.drawImageToRect(canvas, new Rectangle(0, 0, rect.width, rect.height),
+        sourceRect: rect);
+      points.clear();
+      querySelector("#output").append(outCanvas);
+    }
+  });
 }
 
 void main() {
-  querySelector("#input-dir").onChange.listen((e) => getPdfFile());
+  // querySelector("#input-dir").onChange.listen((e) => getPdfFile());
   querySelector("#send-url").onClick.listen((e) => getCardImages());
-  querySelector("#yay").onDragStart.listen((e) => drag(e));
+  // querySelector("#yay").onDragStart.listen((e) => drag(e));
+
+  CanvasElement canvas = querySelector('#myCanvas');
+  ImageElement img = new ImageElement(src: "./love.png");
+  // querySelector("body").append(img);
+  img.onLoad.listen((e) => initCanvas(img, canvas));
 
 //   querySelector("#enterUrl").onclick.listen((event) {});
 }
