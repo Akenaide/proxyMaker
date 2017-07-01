1d8bf8a75c8d9a5ea3ec6d5bd5a14582c10ecb1e
Akenaide <kevinms1@gmail.com>
2017-07-01 03:06:08 +0200

    Try to fix http error

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ import (
 )
 
 const yuyuteiURL = "http://yuyu-tei.jp/"
-const wsDeckURL = "https://wsdecks.com/"
+const wsDeckURL = "https://wsdecks.com"
 const hoTcURL = "http://www.heartofthecards.com/code/cardlist.html?card=WS_"
 const yuyuteiBase = "http://yuyu-tei.jp/game_ws"
 
