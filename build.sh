411e9535315c2c5dda04555f70dd30a7140d99ae
root on eev <root@eev.eelon.moe>
2019-12-28 17:02:24 +0000

    weird stuff

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ pub global activate webdev
 DEST=$1
 
 echo "build to $DEST"
-mkdir -p $DEST
-mkdir -p $DEST/static
 
 webdev build --output $DEST
 
+mkdir -p $DEST
+mkdir -p $DEST/static
 go build -o $DEST
